1a435ff68bfd7a0f5d83fdde3206bb56510c9c5c
João Pedro G. M. Silva <joaopedrogmoraissilva@gmail.com>
2024-11-30 09:36:21 -0300

    criado admin e votacao

--- urna-virtual/urna/settings.py
+++ urna-virtual/urna/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'votacao.apps.VotacaoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
