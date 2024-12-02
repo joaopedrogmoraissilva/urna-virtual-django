f43b89f02a80fc30667b635f628b83f0f000453f
João Pedro G. M. Silva <joaopedrogmoraissilva@gmail.com>
2024-12-02 13:16:44 -0300

    criado o modelo de user

--- urna-virtual/votacao/models.py
+++ urna-virtual/votacao/models.py
@@ -15,3 +15,11 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
