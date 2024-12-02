af5c6758552ae1ec45a25218bab7995ea8597bbc
João Pedro G. M. Silva <joaopedrogmoraissilva@gmail.com>
2024-12-02 13:21:23 -0300

    criado view de registro

--- urna-virtual/votacao/views.py
+++ urna-virtual/votacao/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return HttpResponse("Votação")
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            name = form.cleaned_data.get('name')
+            messages.success(request, f'Account created for {name}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
