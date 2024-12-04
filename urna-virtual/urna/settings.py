4d4eaa13046d24355c42b36b5e95d16f7a1fc055
João Pedro G. M. Silva <joaopedrogmoraissilva@gmail.com>
2024-12-04 05:35:44 -0300

    customizei a pagina de registro usando bootstrap

--- urna-virtual/urna/settings.py
+++ urna-virtual/urna/settings.py
@@ -129,5 +129,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/votacao/'
-LOGOUT_REDIRECT_URL = '/votacao/'
 
+LOGOUT_REDIRECT_URL = '/votacao/'
